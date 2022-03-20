a4d212ad8146d630554432775089aaaee82e824f
Joe Campbell <10763991+needs-coffee@users.noreply.github.com>
2022-03-20 22:09:54 +0000

    Refactoring

--- winbackup/configsaver.py
+++ winbackup/configsaver.py
@@ -17,20 +17,17 @@
 import sys
 import os
 import subprocess
-from send2trash import send2trash
-from colorama import Fore, Back, Style, init
+from colorama import Fore, Style
 import logging
 import shutil
 
 class ConfigSaver:
     def __init__(self, config_save_path=None) -> None:
         """
-        config save path should be an empty folder to save all  config files/folders to.
-        within the winbackup script -> create 'config' folder, save config, 7z config, delete config folder (?send2trash).
+        Config save path should be an empty folder to save all config files/folders to.
+        Within the winbackup script -> create 'config' folder, save config, 7z config, delete config folder..
         """
         self.config_save_path = config_save_path
-        # with importlib.resources.path('winbackup', 'scripts') as p:
-        #     self.winfetch_path = os.path.join(p)
         self.winfetch_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'scripts', 'winfetch.ps1')
         self.winfetch_config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'scripts', 'config.ps1')
         self.installed_prog_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'scripts', 'installed_programs.ps1')
@@ -38,18 +35,19 @@ class ConfigSaver:
 
 
     @staticmethod
-    def _command_runner(shell_commands: list) -> str:
-        return_data = subprocess.run(shell_commands, stdout=subprocess.PIPE).stdout.decode('utf-8',errors='ignore')
-        return return_data
+    def _command_runner(shell_commands:list) -> str:
+        logging.debug(f'Command runner cmds: {shell_commands}')
+        return subprocess.run(shell_commands, stdout=subprocess.PIPE).stdout.decode('utf-8', errors='ignore')
 
 
-    def set_videos_directory_path(self, videos_path) -> None:
+    def set_videos_directory_path(self, videos_path:str) -> None:
+        logging.debug(f'Video path set: {videos_path}')
         self.videos_path = videos_path
 
 
-    def save_config_files(self, out_path=None) -> str:
+    def save_config_files(self, out_path:str=None) -> str:
         """
-        save all the configuration elements.
+        Save all the configuration elements.
         Returns the path the files were saved to.
         """
         if out_path == None:
@@ -62,7 +60,7 @@ class ConfigSaver:
         self.save_vscode_extensions(out_path)
         self.save_path_env(out_path)
         self.save_ssh_directory(out_path)
-        self.save_videos_directory_filenames(out_path)
+        self.save_videos_directory_filenames(out_path, self.videos_path)
         self.save_file_associations(out_path)
         self.save_drivers(out_path)
         self.save_systeminfo(out_path)
@@ -71,158 +69,160 @@ class ConfigSaver:
         return out_path
 
 
-    def save_winfetch(self, out_path) -> None:
+    def save_winfetch(self, out_path:str) -> None:
         winfetch_output = self._command_runner(['powershell.exe', self.winfetch_path, 
                                                 '-noimage', 
                                                 '-stripansi', 
                                                 '-configpath', 
-                                                self.winfetch_config_path]).replace('\r\n','\n')
-        with open(os.path.join(out_path,'winfetch_output.txt'),'w') as out_file:
+                                                self.winfetch_config_path]).replace('\r\n', '\n')
+        with open(os.path.join(out_path,'winfetch_output.txt'), 'w') as out_file:
             out_file.write(winfetch_output)
         print(' > Winfetch saved.')
-        logging.info('config_backup - Winfetch Saved.')
+        logging.info('Winfetch Saved.')
 
 
-    def save_installed_programs(self, out_path) -> None:
-        installed_output = self._command_runner(['powershell.exe', self.installed_prog_path]).replace('\r\n','\n')
-        with open(os.path.join(out_path,'installed_programs.txt'),'w') as out_file:
+    def save_installed_programs(self, out_path:str) -> None:
+        installed_output = self._command_runner(['powershell.exe', self.installed_prog_path]).replace('\r\n', '\n')
+        with open(os.path.join(out_path,'installed_programs.txt'), 'w') as out_file:
             out_file.write(installed_output)
         print(' > Installed Programs saved.')
-        logging.info('config_backup - Installed Programs Saved.')
+        logging.info('Installed Programs Saved.')
 
 
-    def save_global_python_packages(self, out_path) -> None:
+    def save_global_python_packages(self, out_path:str) -> None:
         try:
             py_version = self._command_runner(['powershell.exe', 'python', '-V']).replace('\r\n', '\n')
             pip_output = self._command_runner(['powershell.exe' ,'pip', 'freeze']).replace('\r\n', '\n')
             pip_output = py_version + pip_output
-            with open(os.path.join(out_path, 'python_packages.txt'),'w') as out_file:
+            with open(os.path.join(out_path, 'python_packages.txt'), 'w') as out_file:
                 out_file.write(pip_output)
             print(' > Global Python Packages saved.')
-            logging.info('config_backup - Global Python Packages Saved.')
-        except:
-            print(Fore.RED + ' xx unable to backup global python packages' + Style.RESET_ALL)
-            logging.warning('config_backup - Unable to save global python packages.')
+            logging.info('Global Python Packages Saved.')
+        except Exception as e:
+            print(Fore.RED + ' XX Unable to backup global python packages' + Style.RESET_ALL)
+            logging.warning(f'Unable to save global python packages. Exception: {e}')
 
 
-    def save_choco_packages(self, out_path) -> None:
+    def save_choco_packages(self, out_path:str) -> None:
         try:
-            choco_output = self._command_runner(['powershell.exe', 'choco', 
-                                                'list', '--local-only']).replace('\r\n','\n')
-            with open(os.path.join(out_path, 'choco_packages.txt'),'w') as out_file:
+            choco_output = self._command_runner(['powershell.exe', 
+                                        'choco',  'list', '--local-only']).replace('\r\n', '\n')
+            with open(os.path.join(out_path, 'choco_packages.txt'), 'w') as out_file:
                 out_file.write(choco_output)
             print(' > Choco Packages saved.')
-            logging.info('config_backup - Choco Pacakges Saved.')
-        except:
-            print(Fore.RED + ' xx unable to backup choco packages.' + Style.RESET_ALL)
-            logging.warning('config_backup - Unable to save choco packages.')
+            logging.info('Choco Pacakges Saved.')
+        except Exception as e:
+            print(Fore.RED + ' XX Unable to backup choco packages.' + Style.RESET_ALL)
+            logging.warning(f'Unable to save choco packages. Exception: {e}')
 
 
-    def save_vscode_extensions(self, out_path) -> None:
+    def save_vscode_extensions(self, out_path:str) -> None:
         try:
             choco_output = self._command_runner(['powershell.exe', 'code', '--list-extensions']).replace('\r\n', '\n')
-            with open(os.path.join(out_path, 'vscode_extensions.txt'),'w') as out_file:
+            with open(os.path.join(out_path, 'vscode_extensions.txt'), 'w') as out_file:
                 out_file.write(choco_output)
             print(' > VSCode Extensions saved.')
-            logging.info('config_backup - Vscode extensions Saved.')
-        except:
-            print(Fore.RED + ' xx unable to backup VSCode extensions.' + Style.RESET_ALL)
-            logging.warning('config_backup - Unable to save VSCode extensions packages.')
+            logging.info('VSCode extensions Saved.')
+        except Exception as e:
+            print(Fore.RED + ' XX Unable to backup VSCode extensions.' + Style.RESET_ALL)
+            logging.warning(f'Unable to save VSCode extensions packages. Exception: {e}')
 
 
-    def save_path_env(self, out_path) -> None:
+    def save_path_env(self, out_path:str) -> None:
         path_list = os.environ['PATH'].split(';')
-        with open(os.path.join(out_path,'path.txt'),'w') as out_file:
+        with open(os.path.join(out_path,'path.txt'), 'w') as out_file:
             for path in path_list:
-                fixed_path = path.replace('\\','/')
+                fixed_path = path.replace('\\', '/')
                 out_file.write(fixed_path + '\n')
         print(' > Path env saved.')
-        logging.info('config_backup - Path env Saved.')
+        logging.info('Path env Saved.')
 
 
-    def save_ssh_directory(self, out_path) -> None:
+    def save_ssh_directory(self, out_path:str) -> None:
         try:
-            ssh_path = os.path.join(os.path.expanduser('~'),'.ssh')
+            ssh_path = os.path.join(os.path.expanduser('~'), '.ssh')
             logging.debug(f'ssh path {ssh_path}')
             if os.path.exists(ssh_path):
                 shutil.copytree(ssh_path, os.path.join(out_path, '.ssh'))
                 print(' > .ssh folder saved.')
-                logging.info('config_backup - .ssh folder Saved.')
+                logging.info('.ssh folder Saved.')
             else:
                 print(' > .ssh folder does not exist - skipping')
-                logging.info('config_backup - .ssh folder does not exist, skipping.')
+                logging.info('.ssh folder does not exist, skipping.')
         except Exception as e:
-            logging.debug(f'exception: {e}')
-            print(Fore.RED + ' xx unable to backup .ssh folder.' + Style.RESET_ALL)
-            logging.warning('config_backup - unable to backup .ssh folder.')
+            print(Fore.RED + ' XX Unable to backup .ssh folder.' + Style.RESET_ALL)
+            logging.warning(f'Unable to backup .ssh folder. Exception: {e}')
 
 
-    def save_videos_directory_filenames(self, out_path) -> None:
-        installed_output = self._command_runner(['powershell.exe', 'tree', self.videos_path]).replace('\r\n','\n')
-        with open(os.path.join(out_path, 'videos_tree.txt'),'w') as out_file:
+    def save_videos_directory_filenames(self, out_path:str, videos_path:str) -> None:
+        logging.debug(f'Videos file list directory: {videos_path}')
+        installed_output = self._command_runner(['powershell.exe', 'tree', videos_path]).replace('\r\n', '\n')
+        with open(os.path.join(out_path, 'videos_tree.txt'), 'w') as out_file:
             out_file.write(installed_output)
         print(' > Videos file list saved.')
-        logging.info('config_backup - Video Files list Saved.')
+        logging.info('Video Files list Saved.')
 
 
-    def save_file_associations(self, out_path) -> None:
+    def save_file_associations(self, out_path:str) -> None:
         try:
             file_assoc = self._command_runner(['powershell.exe', 'cmd', '/c', 'assoc']).replace('\r\n', '\n')
             with open(os.path.join(out_path, 'file_associations.txt'), 'w') as out_file:
                 out_file.write(file_assoc)
             print(' > File associations saved.')
-            logging.info('config_backup - file associations Saved.')
-        except:
-            print(Fore.RED + ' xx unable to backup file associations.' + Style.RESET_ALL)
-            logging.warning('config_backup - unable to save file associations.')
+            logging.info('File associations Saved.')
+        except Exception as e:
+            print(Fore.RED + ' XX Unable to backup file associations.' + Style.RESET_ALL)
+            logging.warning(f'Unable to save file associations. Exception: {e}')
 
 
-    def save_drivers(self, out_path) -> None:
+    def save_drivers(self, out_path:str) -> None:
         try:
             drivers = self._command_runner(['powershell.exe', 'driverquery']).replace('\r\n', '\n')
             with open(os.path.join(out_path, 'drivers.txt'), 'w') as out_file:
                 out_file.write(drivers)
             print(' > Drivers saved.')
-            logging.info('config_backup - Drivers Saved.')
-        except:
-            print(Fore.RED + ' xx unable to backup drivers.' + Style.RESET_ALL)
-            logging.warning('config_backup - Unable to save Drivers.')
+            logging.info('Drivers Saved.')
+        except Exception as e:
+            print(Fore.RED + ' XX Unable to backup drivers.' + Style.RESET_ALL)
+            logging.warning(f'Unable to save Drivers. Exception: {e}')
 
 
-    def save_systeminfo(self, out_path) -> None:
+    def save_systeminfo(self, out_path:str) -> None:
         try:
             sysinfo = self._command_runner(['powershell.exe', 'systeminfo']).replace('\r\n', '\n')
             with open(os.path.join(out_path, 'systeminfo.txt'), 'w') as out_file:
                 out_file.write(sysinfo)
             print(' > Systeminfo saved.')
-            logging.info('config_backup - Systeminfo Saved.')
-        except:
-            print(Fore.RED + ' xx unable to backup systeminfo.' + Style.RESET_ALL)
-            logging.warning('config_backup - Unable to save systeminfo.')
+            logging.info('Systeminfo Saved.')
+        except Exception as e:
+            print(Fore.RED + ' XX Unable to backup systeminfo.' + Style.RESET_ALL)
+            logging.warning(f'Unable to save systeminfo. Exception: {e}')
         
 
-    def save_battery_report(self, out_path) -> None:
+    def save_battery_report(self, out_path:str) -> None:
         try:
             shell_commands = ['powershell.exe', 'powercfg', 
                                 '/batteryreport', '/output', 
-                                os.path.join(out_path,'batteryreport.html')]
+                                os.path.join(out_path, 'batteryreport.html')]
             batreport = subprocess.run(
                             shell_commands, 
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE).stderr.decode('utf-8', errors='ignore').replace('\r\n', '\n')
 
             if not 'Unable to perform operation.' in batreport:
-                print(' > battery report saved.')
-                logging.info('config_backup - battery report Saved.')
+                print(' > Battery report saved.')
+                logging.info('Battery report Saved.')
             else:
-                print(Fore.RED + ' xx unable to backup battery report (is this a battery powered device?)' + Style.RESET_ALL)
-                logging.warning('config_backup - Unable to save battery report (is this a battery powered device?).')
-        except:
-            print(Fore.RED + ' xx unable to backup battery report.' + Style.RESET_ALL)
-            logging.warning('config_backup - Unable to save battery report - Exception occured.')        
+                print(Fore.RED + ' XX unable to save battery report (Is this a battery powered device?)' + Style.RESET_ALL)
+                logging.warning('Unable to save battery report (Is this a battery powered device?)')
+        except Exception as e:
+            print(Fore.RED + ' XX unable to backup battery report.' + Style.RESET_ALL)
+            logging.warning(f'Unable to save battery report. Exception: {e}')        
 
 
 if __name__ == '__main__':
+
+    from send2trash import send2trash
     logging.basicConfig(stream= sys.stdout,
                         format='%(asctime)s - %(levelname)s - %(message)s',
                         level = logging.DEBUG)
@@ -240,7 +240,7 @@ if __name__ == '__main__':
 
     print(Fore.GREEN + ' > ' + 'Cleanup?' +' (y/n): ' + Style.RESET_ALL, end='')
     reply = str(input()).lower().strip()
-    if reply.lower() in ('yes', 'y'):
+    if reply.lower() in {'yes', 'y'}:
         send2trash(os.path.join('.', 'tmp_test'))
         print('Cleaned up temp dir.')
     else:
