c1e9b0d948c5abf054e10f47b686baad48bc81d0
Joe Campbell <10763991+needs-coffee@users.noreply.github.com>
2022-06-22 22:11:16 +0100

    import error

--- winbackup/winbackup.py
+++ winbackup/winbackup.py
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see < https: // www.gnu.org/licenses/>.
 
-from email.policy import default
 import os
 import sys
 import signal
